350d7ad225c0dd169d7c1834f3181054e58a8129
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-07-29 17:58:24 +0200

    Added model logic for User and base for controllers in user.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
+import User from "../models/User.js";
+
 export const getUsers = async (req, res) => {
   try {
-    // Some logic here
+    const users = await User.findAll();
+    res.json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
